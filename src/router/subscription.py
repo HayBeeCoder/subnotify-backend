3a43f4390b504e6c83498008583bca17bd7072f0
HayBeeCoder <haybeecodes@gmail.com>
2025-03-26 10:18:55 +0100

    [feature] implement response_model for each route and updated the db parameter of functions that are necessary to be updated

--- src/router/subscription.py
+++ src/router/subscription.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, Depends
 from data.init import get_db
-from schemas.subscription import Subscription
+from schemas.subscription import Subscription, SubscriptionsResponse
 import service.subscription as service
 from sqlalchemy.orm.session import Session
 
 router = APIRouter(prefix="/subscription", tags=["subscription"])
 
 @router.get("/")
-def getAllService()-> list[Subscription]:
-    return service.get_all()
+def getAllService(db: Session = Depends(get_db)) -> list[Subscription]:
+    return service.get_all(db)
 
 @router.get("/{service_provider_or_type}")
-def getService(service_provider_or_type: str) -> Subscription:
-    return service.get_one(service_provider_or_type)
+def getService(service_provider_or_type: str, db: Session = Depends(get_db)) -> Subscription:
+    return service.get_one(service_provider_or_type,db)
 
 @router.post("/")
 def create(subscription: Subscription, db: Session = Depends(get_db)) -> Subscription:
@@ -20,8 +20,8 @@ def create(subscription: Subscription, db: Session = Depends(get_db)) -> Subscri
 
 
 @router.delete("/{service_id}")
-def delete(id: str):
-    return service.delete(id)
+def delete(service_id: str, db: Session = Depends(get_db)) -> Subscription:
+    return service.delete(service_id,db)
 
 
 
