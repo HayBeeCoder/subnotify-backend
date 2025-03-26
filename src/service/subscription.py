fe4e891fdccea55d9622f99684f4e8fdf811cae6
HayBeeCoder <haybeecodes@gmail.com>
2025-03-26 05:32:51 +0100

    [feature] implement  the service layer

--- src/service/subscription.py
+++ src/service/subscription.py
@@ -6,18 +6,18 @@ from schemas.subscription import Subscription
 import data.subscription as data 
 from data.init import get_db
 
-# def get_all() -> list[Subscription]:
-#     return data.get_all()
+def get_all() -> list[Subscription]:
+    return data.get_all()
 
-# def get_one(name: str) -> Subscription:
-#     return data.get_one(name)
+def get_one(service_provider_or_type: str) -> Subscription:
+    return data.get_one(service_provider_or_type)
 
 def create(subscription: Subscription, db: Session ) -> Subscription:
 
     return data.create( subscription, db)
 
-# def modify(subscription: Subscription) -> Subscription:
-#     return data.modify(subscription)
+def modify(subscription: Subscription) -> Subscription:
+    return data.modify(subscription)
 
-# def delete(name: str) -> bool:
-#     return data.delete(name)
+def delete(id: str) -> bool:
+    return data.delete(id)
