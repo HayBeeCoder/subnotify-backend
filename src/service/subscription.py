63e0812fe4eecf8f06d3e35552d987a3fe784604
HayBeeCoder <haybeecodes@gmail.com>
2025-03-26 10:15:46 +0100

    [feature] update service layer

--- src/service/subscription.py
+++ src/service/subscription.py
@@ -4,20 +4,19 @@ from sqlalchemy.orm.session import Session
 
 from schemas.subscription import Subscription
 import data.subscription as data 
-from data.init import get_db
 
-def get_all() -> list[Subscription]:
-    return data.get_all()
 
-def get_one(service_provider_or_type: str) -> Subscription:
-    return data.get_one(service_provider_or_type)
+def get_all(db: Session) -> list[Subscription]:
+    return data.get_all(db)
+
+def get_one(service_provider_or_type: str, db: Session) -> Subscription:
+    return data.get_one(service_provider_or_type, db)
 
 def create(subscription: Subscription, db: Session ) -> Subscription:
-
     return data.create( subscription, db)
 
-def modify(subscription: Subscription) -> Subscription:
-    return data.modify(subscription)
+def modify(subscription: Subscription, db: Session) -> Subscription:
+    return data.modify(subscription, db)
 
-def delete(id: str) -> bool:
-    return data.delete(id)
+def delete(service_id: str, db: Session) -> Subscription:
+    return data.delete(service_id,db)
