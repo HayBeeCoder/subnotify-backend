573459dca684f89f680f99358ac6cd3bc6fdae09
HayBeeCoder <haybeecodes@gmail.com>
2025-03-29 13:43:38 +0100

    [feature] implement cors and scheduler

--- src/main.py
+++ src/main.py
@@ -1,11 +1,51 @@
+import datetime
+import os
+
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from db import models
 from router import subscription as subscriptionrouter
 from data.init import engine
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from supabase import create_client, Client
 
-app = FastAPI()
+
+
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+scheduler = BackgroundScheduler()
+
+def scheduled_task():
+    """This function runs at scheduled intervals."""
+    print(f"Task executed at: {datetime.datetime.now()}")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Handles startup and shutdown events."""
+    print("Starting scheduler...")
+    scheduler.start()
+    scheduler.add_job(scheduled_task, IntervalTrigger(seconds=10), id="interval_task", replace_existing=True)
+    yield
+    print("Shutting down scheduler...")
+    scheduler.shutdown()
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(subscriptionrouter.router)
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 if __name__ == "__main__":
