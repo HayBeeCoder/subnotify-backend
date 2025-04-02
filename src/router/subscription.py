d23087af7928dd36fd6e5b6af423ef54e48a8ea8
HayBeeCoder <haybeecodes@gmail.com>
2025-04-03 00:40:09 +0100

    [feature] make the get_all function enable query string and also sorting

--- src/router/subscription.py
+++ src/router/subscription.py
@@ -1,28 +1,49 @@
-from fastapi import APIRouter, Depends, Header
+from typing import Optional
+from fastapi import APIRouter, Depends, Header,Query
 from data.init import get_db
-from schemas.subscription import CreateSubscriptionResponse, Subscription, SubscriptionsResponse
+from user import get_current_user
+from schemas.subscription import (
+    CreateSubscriptionResponse,
+    GetAllSubscriptionsResponse,
+    Subscription,
+    SubscriptionsResponse,
+)
 import service.subscription as service
 from sqlalchemy.orm.session import Session
 
 
-router = APIRouter(prefix="/subscription", tags=["subscription"])
+router = APIRouter(prefix="/subscription", tags=["subscription"], dependencies=[Depends(get_current_user)])
 
-@router.get("/")
-def getAllService(db: Session = Depends(get_db)) -> list[Subscription]:
-    return service.get_all(db)
+
+# @router.get("/subscriptions")
+@router.get("/all")
+@router.get("/all/")
+def get_all(
+    authorization: str = Header(None),
+    q: Optional[str] = Query(
+        None, description="Search query for filtering subscriptions"
+    ),
+    sort: Optional[str] = Query(
+        None, description="Sort order: az, za, new, old, long, short"
+    ),
+) -> GetAllSubscriptionsResponse:
+    return service.get_all(authorization,q, sort)
+
 
 @router.get("/{service_provider_or_type}")
-def getService(service_provider_or_type: str, db: Session = Depends(get_db)) -> Subscription:
-    return service.get_one(service_provider_or_type,db)
+def getService(
+    service_provider_or_type: str, db: Session = Depends(get_db)
+) -> Subscription:
+    return service.get_one(service_provider_or_type, db)
+
 
 @router.post("/")
-def create(subscription: Subscription, authorization: str = Header(None)) -> CreateSubscriptionResponse:
-    return service.create(subscription,authorization)
+def create(
+    subscription: Subscription, authorization: str = Header(None)
+) -> CreateSubscriptionResponse:
+    return service.create(subscription, authorization)
 
 
 @router.delete("/{service_id}")
 def delete(service_id: str, db: Session = Depends(get_db)) -> Subscription:
-    return service.delete(service_id,db)
-
-
-
+    return service.delete(service_id, db)
