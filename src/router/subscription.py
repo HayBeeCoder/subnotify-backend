07a31bf759848c952272b6da927edefa953991a8
HayBeeCoder <haybeecodes@gmail.com>
2025-04-03 05:26:48 +0100

    [feature] implement functionality for modifying a subscription in the router layer

--- src/router/subscription.py
+++ src/router/subscription.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import APIRouter, Depends, Header,Query
+from fastapi import APIRouter, Depends, Header, Query
 from auth.auth import get_authenticated_user
 from data.init import get_db
 from user import get_current_user
@@ -8,19 +8,24 @@ from schemas.subscription import (
     GetAllSubscriptionsResponse,
     Subscription,
     SubscriptionsResponse,
+    UpdateSubscriptionResponse,
 )
 import service.subscription as service
 from sqlalchemy.orm.session import Session
 
 
-router = APIRouter(prefix="/subscription", tags=["subscription"], dependencies=[Depends(get_current_user)])
+router = APIRouter(
+    prefix="/subscription",
+    tags=["subscription"],
+    dependencies=[Depends(get_current_user)],
+)
 
 
 # @router.get("/subscriptions")
 @router.get("/all")
-@router.get("/all/")
+# @router.get("/all/")
 def get_all(
-user: dict = Depends(get_authenticated_user),
+    user: dict = Depends(get_authenticated_user),
     q: Optional[str] = Query(
         None, description="Search query for filtering subscriptions"
     ),
@@ -28,7 +33,7 @@ user: dict = Depends(get_authenticated_user),
         None, description="Sort order: az, za, new, old, long, short"
     ),
 ) -> GetAllSubscriptionsResponse:
-    return service.get_all(user,q, sort)
+    return service.get_all(user, q, sort)
 
 
 @router.get("/{service_provider_or_type}")
@@ -40,11 +45,20 @@ def getService(
 
 @router.post("/")
 def create(
-    subscription: Subscription, 
-    user: dict = Depends(get_authenticated_user)
+    subscription: Subscription, user: dict = Depends(get_authenticated_user)
 ) -> CreateSubscriptionResponse:
     return service.create(subscription, user)
 
+
+@router.put("/{subscription_id}")
+def modify(
+    subscription_id: int,
+    subscription: Subscription,
+    user: dict = Depends(get_authenticated_user),
+) -> UpdateSubscriptionResponse:
+    return service.modify(subscription_id, subscription, user)
+
+#   "end_date": 1714502400,
 
 @router.delete("/{service_id}")
 def delete(service_id: str, db: Session = Depends(get_db)) -> Subscription:
