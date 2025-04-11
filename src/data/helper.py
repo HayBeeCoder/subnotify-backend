de015bb48cba9bde4ceb2b9362b991fea413c886
HayBeeCoder <haybeecodes@gmail.com>
2025-04-11 11:39:45 +0100

    [feature] include the user's name and email in the register too

--- src/data/helper.py
+++ src/data/helper.py
@@ -25,10 +25,13 @@ from datetime import datetime
 
 from supabase import Client
 
+from gotrue.types import User
+
 
 def add_to_due_register(
-    subscription_id: int, user_id: str, end_date: str, supabase: Client
+    subscription_id: int, user: User, end_date: str, supabase: Client
 ):
+    user_id = user.id
     deadline = end_date - 86400  # deadline is 24hrs before the end_date
 
     date_key = datetime.fromtimestamp(deadline).strftime("%Y-%m-%d")
@@ -40,10 +43,22 @@ def add_to_due_register(
 
     if date_key in due_dates:
         due_dates[date_key].append(
-            {"subscription_id": subscription_id, "user_id": user_id}
+            {
+                "subscription_id": subscription_id,
+                "user_id": user_id,
+                "email": user.user_metadata["email"],
+                "name": user.user_metadata["full_name"],
+            }
         )
     else:
-        due_dates[date_key] = [{"subscription_id": subscription_id, "user_id": user_id}]
+        due_dates[date_key] = [
+            {
+                "subscription_id": subscription_id,
+                "user_id": user_id,
+                "email": user.user_metadata["email"],
+                "name": user.user_metadata["full_name"],
+            }
+        ]
 
     supabase.table("subscription_due_dates").update({"due_dates": due_dates}).eq(
         "id", 1
@@ -84,12 +99,15 @@ def update_enddate(
     new_date_key = datetime.fromtimestamp(new_deadline).strftime("%Y-%m-%d")
 
     result = (
-        supabase.table("subscription_due_dates").select("id, due_dates").single().execute()
+        supabase.table("subscription_due_dates")
+        .select("id, due_dates")
+        .single()
+        .execute()
     )
     # due_dates = result.data["due_dates"]
 
     due_dates = result.data.get("due_dates", {})  # Ensure it's a dictionary
-        # Remove subscription from the old date key
+    # Remove subscription from the old date key
     subscription_entry = None
     if old_date_key in due_dates:
         for i, obj in enumerate(due_dates[old_date_key]):
@@ -101,7 +119,6 @@ def update_enddate(
     if not bool(len(due_dates[old_date_key])):
         del due_dates[old_date_key]
 
-
     if subscription_entry:
         if new_date_key not in due_dates:
             due_dates[new_date_key] = []
@@ -111,9 +128,9 @@ def update_enddate(
         # Update the record in the database
 
     update_result = (
-            supabase.table("subscription_due_dates")
-            .update({"due_dates": due_dates})
-            .eq("id", result.data["id"])
-            .execute()
-        )
+        supabase.table("subscription_due_dates")
+        .update({"due_dates": due_dates})
+        .eq("id", result.data["id"])
+        .execute()
+    )
     # print({"message": "End date updated successfully", "due_dates": due_dates})
